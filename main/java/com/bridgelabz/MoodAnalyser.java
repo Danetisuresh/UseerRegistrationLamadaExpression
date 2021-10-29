d4bc9a774266aa1944af9fb443e92f72cc73231b
suresh Daneti <sureshdaneti99@gmail.com>
2021-10-29 10:48:47 +0530

    Added: to validate all the use cases through exception handling

--- main/java/com/bridgelabz/MoodAnalyser.java
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,14 +1,23 @@
 package com.bridgelabz;
 /**
- * Purpose  - Mood Analyser for user
+ * Purpose  - To validate the mood of the user and handling the exception
  */
+
 public class MoodAnalyser {
-	public static String analyseMood(String mood) {
-		if (mood.toLowerCase().contains("happy")) {
-			return "Entry Successful";
-		} else if (mood.toLowerCase().contains("sad")) {
-			return "Entry Failed";
-		} else return null;
-	}
+	    public static String analyseMood(String mood) throws MoodAnalyserException {
+	        try {
+	            if (mood.length() == 0) {
+	                throw new MoodAnalyserException(MoodAnalyserException.type.EMPTY, "Invalid Input");
+	            } else if (mood.toLowerCase().contains("happy")) {
+	                return "Entry Successful";
+	            } else if (mood.toLowerCase().contains("sad")) {
+	                return "Entry Failed";
+	            } else {
+	                return "Invalid Input";
+	            }
+	        } catch (NullPointerException e) {
+	            throw new MoodAnalyserException(MoodAnalyserException.type.NULL, "Invalid mood");
+	        }
+	    }
 
 }
--- main/java/com/bridgelabz/UserRegistrationException.java
+++ main/java/com/bridgelabz/UserRegistrationException.java
@@ -1,5 +1,21 @@
 package com.bridgelabz;
 
-public class UserRegistrationException {
+public class UserRegistrationException extends Exception {
+	String message;
+	public static ExceptionType type;
+
+	public enum ExceptionType {
+		NULL, EMPTY;
+	}
+
+	public UserRegistrationException(ExceptionType type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return message;
+	}
 
 }
